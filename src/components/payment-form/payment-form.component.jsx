f00b027a6ce1a587e3e5198aff90d1deaa64d5ef
Yolanda Collins <landa.collins14@gmail.com>
2023-06-10 21:26:19 +0200

    Converting to typescript

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -15,11 +15,44 @@ const PaymentForm = () => {
         if (!stripe || !elements) {
             return;
         }
+
+        const response = await fetch('/.netlify/functions/create-payment-intent', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ amount: 100 }),
+        }).then((res) => {
+            return res.json();
+        });
+
+        const {
+            paymentIntent: { client_secret },
+        } = response;
+
+        console.log(response);
+
+        const paymentResult = await stripe.confirmCardPayment(client_secret, {
+            payment_method: {
+                card: elements.getElement(CardElement),
+                billing_details: {
+                    name: 'Yolanda Turner',
+                },
+            },
+        });
+
+        if (paymentResult.error) {
+            alert(paymentResult.error.message);
+        } else {
+            if (paymentResult.paymentIntent.status === 'succeeded') {
+                alert('Payment Successful');
+            }
+        }
     };
 
     return (
         <PaymentFormContainer>
-            <FormContainer>
+            <FormContainer onSubmit={paymentHandler}>
                 <h2>Credit Card Payment</h2>
                 <CardElement />
                 <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay Now</Button>
